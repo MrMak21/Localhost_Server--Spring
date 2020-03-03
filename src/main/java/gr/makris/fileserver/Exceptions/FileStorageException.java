bb5cf08e0e3b51a9bce2387991b2d2003d35adfa
Panagiotis Makris <panosmakris7@windowslive.com>
2020-03-03 16:51:53 +0200

    Move project to git - Ready to play

---
+++ src/main/java/gr/makris/fileserver/Exceptions/FileStorageException.java
@@ -0,0 +1,11 @@
+package gr.makris.fileserver.Exceptions;
+
+public class FileStorageException extends RuntimeException {
+    public FileStorageException(String message) {
+        super(message);
+    }
+
+    public FileStorageException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
